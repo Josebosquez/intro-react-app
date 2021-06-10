a146aae3c9efe77f7b7582669cc009f60223fd2e
Josebosquez <77699516+Josebosquez@users.noreply.github.com>
2021-06-10 08:33:33 -0600

    side bar completed

--- src/App.js
+++ src/App.js
@@ -3,40 +3,49 @@ import  './App.css'
 
 export class App extends Component {
   state = {
-    colorList: [
-      {
-        id: 1,
-        color: 'gold',
-      },
-      {
-        id: 2,
-        color: 'lightblue',
-      },
-      {
-        id: 3,
-        color: 'purple',
-      },
-    ]
+    colorList: ["gold", "lightblue", "purple"],
+    backgroundColorInput: "",
+  };
+
+  handleOnBackgroundChange=(color) => {
+    // console.log(color)
+    this.setState ({
+      backgroundColorInput: color,
+    })
   }
-  
-  
-  
+
   render() {
     return (
-      <div className={'all'}> 
-        <div className={'header'}>
-          Jose's Header
-        </div>
+      <div className={"all"}>
+        <div className={"header"}>Jose's Header</div>
 
-        <div className={'center'}>
-          <div className={'sidebar'}>
-            SideBar
+        <div className={"center"}>
+          <div
+            className="sidebar"
+            style={{ backgroundColor: this.state.backgroundColorInput }}
+          >
+            <h3> SideBar</h3>
+            <ul className={"color"}>
+              {this.state.colorList.map((item, index) => { // map creates a new array based on what we are looking for.
+                return (
+                  <li
+                    key={index}
+                    onClick={() => this.handleOnBackgroundChange(item)}
+                  >
+                    {item}
+                  </li> // the new array will give each color a onClick function that will take in handle the function handleOnBackgroundChange.
+                );
+              })}
+            </ul>
           </div>
-          <div className={'body'}>
-            <h1>Body</h1>
-            <div>Image</div>
-            <p>This is my description paragraph.</p>
-            <div className={'squares'}>
+          <div className={"body"}>
+            <h3>Body</h3>
+            <img
+              className={"img"}
+              src="https://i0.wp.com/www.animemaru.com/wp-content/uploads/2018/08/dont_lewd_lolis.jpg?w=640&ssl=1"
+            />
+            <p>This is the Loli Police.</p>
+            <div className={"squares"}>
               <div>square 1</div>
               <div>square 2</div>
               <div>square 3</div>
@@ -44,15 +53,13 @@ export class App extends Component {
           </div>
         </div>
 
-      <div className={'footer'}>
-        <h1 className={'footer-h1'}>Copyright Jose</h1>
-        <h1>Footer</h1>
-        <h1 className={'empty'}>Empty for display purposes</h1>
-
+        <div className={"footer"}>
+          <h1 className={"footer-h1"}>Copyright Jose</h1>
+          <h3>Footer</h3>
+          <h1 className={"empty"}>Empty for display purposes</h1>
+        </div>
       </div>
-
-      </div>
-    )
+    );
   }
 }
 
